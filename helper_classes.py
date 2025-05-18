a592e59173f661bcd603546dba40d481df2cb32b
gonhonig <gon.honig@post.runi.ac.il>
2025-05-18 11:29:34 +0300

    fixed shadow error

--- helper_classes.py
+++ helper_classes.py
@@ -45,7 +45,7 @@ class DirectionalLight(LightSource):
 
     # This function returns the ray that goes from the light source to a point
     def get_light_ray(self, intersection_point):
-        return Ray(intersection_point, -normalize(self.direction))
+        return Ray(intersection_point, -self.direction)
 
     # This function returns the distance from a point to the light source
     def get_distance_from_light(self, intersection):
@@ -66,7 +66,7 @@ class PointLight(LightSource):
 
     # This function returns the ray that goes from a point to the light source
     def get_light_ray(self, intersection):
-        return Ray(intersection, normalize(self.position - intersection))
+        return Ray(intersection, self.position - intersection)
 
     # This function returns the distance from a point to the light source
     def get_distance_from_light(self, intersection):
@@ -90,7 +90,7 @@ class SpotLight(LightSource):
 
     # This function returns the ray that goes from the light source to a point
     def get_light_ray(self, intersection):
-        return Ray(intersection, normalize(self.position - intersection))
+        return Ray(intersection, self.position - intersection)
 
     def get_distance_from_light(self, intersection):
         return np.linalg.norm(self.position - intersection)
@@ -147,7 +147,7 @@ class Object3D:
 class Plane(Object3D):
     def __init__(self, normal, point):
         super(Object3D).__init__()
-        self.normal = np.array(normal)
+        self.normal = normalize(np.array(normal))
         self.point = np.array(point)
 
     def intersect(self, ray: Ray):
@@ -229,13 +229,13 @@ class Diamond(Object3D):
                 \/*/
                  E
 
-    Similar to Traingle, every from face of the diamond's faces are:
+    Similar to Triangle, every from face of the diamond's faces are:
         A -> B -> D
         B -> C -> D
-        A -> C -> B
+        C -> A -> D
         E -> B -> A
         E -> C -> B
-        C -> E -> A
+        E -> A -> C
     """
 
     def __init__(self, v_list):
@@ -244,24 +244,33 @@ class Diamond(Object3D):
         self.triangle_list = self.create_triangle_list()
 
     def create_triangle_list(self):
-        l = []
         t_idx = [
             [0, 1, 3],
             [1, 2, 3],
-            [0, 3, 2],
+            [2, 0, 3],
             [4, 1, 0],
             [4, 2, 1],
-            [2, 4, 0]]
-        # TODO
-        return l
+            [4, 0, 2]]
+
+        return [Triangle(*self.v_list[i]) for i in t_idx]
 
     def apply_materials_to_triangles(self):
-        # TODO
-        pass
+        for triangle in self.triangle_list:
+            triangle.set_material(self.ambient, self.diffuse, self.specular, self.shininess, self.reflection)
 
     def intersect(self, ray: Ray):
-        # TODO
-        pass
+        nearest_triangle: Object3D = None
+        min_distance = np.inf
+        nearest_triangle_normal = None
+
+        for triangle in self.triangle_list:
+            _, distance, normal = triangle.intersect(ray)
+            if distance is not None and distance < min_distance:
+                min_distance = distance
+                nearest_triangle = triangle
+                nearest_triangle_normal = normal
+
+        return nearest_triangle, min_distance, nearest_triangle_normal
 
 
 class Sphere(Object3D):
@@ -271,35 +280,61 @@ class Sphere(Object3D):
         self.radius = radius
 
     def intersect(self, ray: Ray):
-        # TODO
-        pass
+        oc = ray.origin - self.center
+        d = ray.direction
+        a = 1
+        b = 2 * np.dot(oc, d)
+        c = np.dot(oc, oc) - self.radius ** 2
+        discriminant = b * b - 4 * a * c
+
+        if discriminant < 0:
+            return None, None, None
+
+        t1, t2 = np.roots([a, b, c])
+        t1 = t1 if t1 > 0 else np.inf
+        t2 = t2 if t2 > 0 else np.inf
+        t = min(t1, t2)
+
+        if t == np.inf:
+            return None, None, None
+
+        hit = ray.origin + t * ray.direction
+        normal = normalize(hit - self.center)
+
+        return self, t, normal
 
 
 def get_color(ray: Ray, ambient, lights: list[LightSource], objects: list[Object3D], level: int):
-    color = np.zeros(3)
+    color = np.zeros(3, dtype=np.float64)
     obj, t, N = ray.nearest_intersected_object(objects)
 
     if obj is None:
         return color
 
-    hit = ray.origin + t * ray.direction
-    color = obj.ambient * ambient
+    hit = ray.origin + t * ray.direction + N * 1e-4
+    color = np.array(obj.ambient * ambient, dtype=np.float64)
 
     for light in lights:
-        shadow_ray = light.get_light_ray(hit + N * 1e-4)
-        shadow_obj, shadow_t, _ = shadow_ray.nearest_intersected_object(objects)
-        light_distance = light.get_distance_from_light(hit)
-        shadow_distance = light.get_distance_from_light(shadow_ray.origin + shadow_t * shadow_ray.direction)
+        shadow_ray = light.get_light_ray(hit)
+        shadow_obj, shadow_t, shadow_N = shadow_ray.nearest_intersected_object(objects)
 
-        if shadow_obj is not None and shadow_distance < light_distance:
-            continue
+        if shadow_obj is not None:
+            shadow_hit = shadow_ray.origin + shadow_t * shadow_ray.direction + shadow_N * 1e-4
+            light_distance = light.get_distance_from_light(hit)
+            shadow_distance = np.linalg.norm(hit - shadow_hit)
+            if shadow_distance < light_distance:
+                continue
 
         intensity = light.get_intensity(hit)
-        L = normalize(shadow_ray.direction)
+        L = shadow_ray.direction
         R = reflected(-L, N)
 
         diffuse = obj.diffuse * intensity * max(0, np.dot(L, N))
-        specular = obj.specular * intensity * max(0, np.dot(R, normalize(ray.direction)) ** obj.shininess)
+        specular = obj.specular * intensity * max(0, np.dot(R, ray.direction) ** obj.shininess)
         color += diffuse + specular
+
+    if level > 1 and obj.reflection > 0:
+        reflected_ray = Ray(hit + N * 1e-4, normalize(reflected(ray.direction, N)))
+        color += obj.reflection * get_color(reflected_ray, ambient, lights, objects, level - 1)
 
     return  color
